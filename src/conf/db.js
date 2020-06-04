32e1e7d4c65eafd7faffdc559d0ce1b77f5717cd
Shouganaii <frias.caio@yahoo.com.br>
2020-06-04 18:44:11 -0300

    first commit

---
+++ src/conf/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const host = "mongo";
+const port = "27017";
+
+mongoose.connect(`mongodb://${host}:${port}/email`).then(connect => {
+    console.log("==> conexão com o MONGO OK!");
+}).catch(err => {
+    console.log("==> falha na conexão com o MongoDB!");
+});
+
+module.exports = mongoose;
---
+++ src/jobs/persistencia.js
@@ -0,0 +1,23 @@
+const redis = require("../conf/redis");
+const emailModel = require("../Models/emailModel");
+const schedule = require("node-schedule");
+
+let exec = 0;
+persistir = async () => {
+    await redis.smembers("sendEmail", function (err, values) {
+        if (!err)
+            for (i in values) {
+                let value = values[i];
+                let email = JSON.parse(value);
+                emailModel.create(email);
+
+                redis.SREM("sendEmail", value);
+            }
+    });
+};
+
+const job = schedule.scheduleJob("0-59/5 * * * * *", async date => {
+    exec += 1;
+    await persistir();
+    console.log(`execução número:${exec}, hora:${date}`);
+});
